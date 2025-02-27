10e8f2fefd51c9c36037535513b934d643eac82e
Abdul-aziz9 <ooshinberu@gmail.com>
2025-02-27 16:53:37 -0500

    Added template extending with post_detail to show each post

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,5 @@ from . import views  # Import the view function
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # URL for listing posts
-    path('post/<int:pk>/', views.post_list, name='post_list')
+    path('<int:pk>/', views.post_detail, name='post_detail')
 ]
