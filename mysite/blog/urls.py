69a5d4df3948f170a5095635530587dbbfbc1977
Abdul-aziz9 <ooshinberu@gmail.com>
2025-02-27 15:40:36 -0500

    Added template extending with base.html

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views  # Import the view function
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # URL for listing posts
+    path('post/<int:pk>/', views.post_list, name='post_list')
 ]
